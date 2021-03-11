5473977a5bf8e5290a3deae43efd2a93b3739f48
CharlesRandom <carlos.rivera.cabrera@gmail.com>
2021-03-10 20:38:25 -0600

    done. Update CountryDetails functional component

--- starter-code/src/Routes.js
+++ starter-code/src/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import CountryDetail from './components/CountryDetail';
+// import CountryDetail from './components/CountryDetail';
+import CountryDetailsFun from './components/CountryDetailsFun';
 
 const Routes = () => (
   <Switch>
     <Route exact path='/' component={()=> (<div>Select a country</div>) }/>
-    <Route path='/country/:id' component={CountryDetail}/>
+    {/* <Route path='/country/:id' component={CountryDetail}/> */}
+    <Route path='/country/:id' component={CountryDetailsFun}/>
   </Switch>
 )
 
